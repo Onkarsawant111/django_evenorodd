350886b2a41fb26461eedcd9d74297f85b625d95
onkar <onkarsawant111@gmail.com>
2024-06-04 22:38:36 +0530

    updated

--- onkar_app/views.py
+++ onkar_app/views.py
@@ -4,17 +4,26 @@ from django.http import HttpResponse
 # Create your views here.
 def onkar(request):
     return HttpResponse("<h1>even or odd at `evenodd route`</h1>")
+    
 def evenodd(request):
     content = {} 
+    s = " "
     try:
         if request.method == "POST":
+            # manual form validation : 
+            if request.POST.get('num')=="": 
+                return render(request, 'onkar.html',{'error':True})
+
             num = eval(request.POST.get('num'))
             r = num % 2
             if r == 0:
                 s = "even"
-            else:
-                s = "odd"        
+            elif r != 0:
+                s = "odd"   
     except:
-        pass
+        if TypeError:
+            s = "only numbers are allowed"
+        elif ValueError:
+            s = "cannot be empty"
     content = {'value':s}
     return render(request, 'onkar.html', content)
